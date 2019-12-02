ae7ff45d45a1381d537effba2f1eb8c9b3a3f7a1
noreaster <nw.northeaster345@gmail.com>
2019-12-02 13:59:15 -0500

    moved the code into a submodule, instead of out of the notebook format

--- image_manipulation/img_manip.py
+++ image_manipulation/img_manip.py
@@ -1,13 +1,14 @@
 import numpy as np
 from scipy import interpolate
 from scipy.ndimage import morphology
+from scipy.stats import multivariate_normal
 from PIL import Image, ImageDraw
-import cv2
+from cv2 import GaussianBlur, blur, getPerspectiveTransform, warpPerspective
 
 def rand_spline(dim,inPts= None, nPts = 5,random_seed =None,startEdge = True,endEdge = True):
     np.random.seed(seed=random_seed)
 
-    # print(dim)
+#     print(nPts)
     invDim = (dim[1],dim[0]) # have to invert the size dim because rows cols is yx vs xy
     if inPts is None:
         inPts = np.concatenate((np.random.randint(invDim[0],size=(nPts,1)),
@@ -43,13 +44,93 @@ def rand_spline(dim,inPts= None, nPts = 5,random_seed =None,startEdge = True,end
     distXY = np.sqrt(np.sum(np.diff(inPts,axis=0)**2,axis=1))
     cdXY = np.concatenate((np.zeros((1)),np.cumsum(distXY)),axis=0)
     iDist = np.arange(np.floor(cdXY[-1])+1)
-    print(max(iDist),cdXY)
+#     print(max(iDist),cdXY)
     splXY = interpolate.pchip_interpolate(cdXY,inPts,iDist)
     return splXY
 
+def rand_gauss(dim,zeroToOne = False, maxCov = 2500, nNorms = 25, random_seed = None,centXY = None,
+              minMaxX = None, minMaxY = None, minCovScale = .1,minDiagCovScale = .25, maxCrCovScale = .7):
+    np.random.seed(seed=random_seed)
+    invDim = (dim[1],dim[0])
+     
+    if zeroToOne == True:
+        xV = np.linspace(0,1,num= dim[0])
+        yV = np.linspace(0,1,num= dim[1])
+        if minMaxX is None:
+            minMaxX = [0,1]
+        if minMaxY is None:
+            minMaxY = [0,1]
+    else:
+        xV = np.arange(dim[0])
+        yV = np.arange(dim[1])
+        
+        if minMaxX is None:
+            minMaxX = [0,invDim[0]]
+        if minMaxY is None:
+            minMaxY = [0,invDim[1]]
+
+    xM, yM = np.meshgrid(xV,yV)
+    pos = np.dstack((xM, yM))
+    
+    if centXY is None:
+        centXY = np.concatenate((np.random.uniform(minMaxX[0],minMaxX[1],size=(nNorms,1)),
+                        np.random.uniform(minMaxY[0],minMaxY[1],size=(nNorms,1))), axis=1)
+    
+    sumMap = np.zeros(invDim)
+    for i in range(nNorms):
+        cent = centXY[i,:]
+        cov = np.zeros((2,2))
+        # need to make a symmetric positive semidefinite covariance matrix
+        cMaxCov = np.random.uniform(maxCov* minCovScale,maxCov,size=(1,1))
+        cov = np.diag(np.random.uniform(cMaxCov * minDiagCovScale,cMaxCov,size=(1,2)).flatten())
+        maxCrCov = np.sqrt(np.product(np.diag(cov)))
+        cov[[0,1],[1,0]] = np.random.uniform(-maxCrCov* maxCrCovScale,maxCrCov*maxCrCovScale) 
+        rv = multivariate_normal(cent.flatten(), cov)
+        
+        sumMap += (rv.pdf(pos) * (cMaxCov * 2 * np.pi))
+
+    sumMap = sumMap * maxCov
+    return sumMap
+
+def add_marker(input_im,random_seed = None,nPts = 4, sampSpl = None, inPts = None, 
+              width = 50, alpha = .75, rgbVal= None,
+              rgbRange = np.array([[0,100],[0,100],[0,100]])):
+    np.random.seed(seed=random_seed)
+    if rgbVal is None:
+        rgbVal = np.zeros((3,1))
+        for i in range(3):
+            rgbVal[i] = np.random.randint(rgbRange[i,0],rgbRange[i,1])
+        rgbVal = rgbVal.flatten()
+
+    dim = input_im.size # width by height
+    invDim = (dim[1],dim[0]) # have to invert the size dim because rows cols is yx vs xy
+#     print(sampSpl is None, inPts is None)
+    if sampSpl is None:
+        if inPts is None:
+            sampSpl = rand_spline(dim, nPts = nPts,random_seed = random_seed)
+        else:
+            sampSpl = rand_spline(dim, inPts = inPts,random_seed = random_seed)
+    
+#     print(nPts)
+    
+    mask = np.ones(dim)
+    mask[(sampSpl[:,1].astype(int)),sampSpl[:,0].astype(int)] = 0
+    bw_dist = morphology.distance_transform_edt(mask)
+
+    bw_reg = bw_dist <= width
+    im_rgba = input_im.convert("RGBA")
+    alpha_mask = Image.fromarray((bw_reg*alpha*255).astype(np.uint8),'L')
+    color_arr = np.zeros((dim[0],dim[1],3),dtype=np.uint8)
+    for i in range(len(rgbVal)):
+        color_arr[:,:,i] = rgbVal[i]
+    color_layer = Image.fromarray(color_arr,'RGB')
+
+    comp_im = Image.composite(color_layer, im_rgba, alpha_mask)
+    return comp_im
+
 
 def add_fold(input_im,samp_arr =None, sampSpl=None, inPts = None,random_seed =None,scaleXY =[1,1],fold_width = 100,
-             samp_shiftXY = None,randEdge=False, nLayers = 1):
+             samp_shiftXY = None,randEdge=False, nLayers = 1, nPts = 3,endEdge = -2):
     np.random.seed(seed=random_seed)
 
     im_arr = np.array(input_im)
@@ -58,9 +139,9 @@ def add_fold(input_im,samp_arr =None, sampSpl=None, inPts = None,random_seed =No
 
     if sampSpl is None:
         if inPts is None:
-            sampSpl = rand_spline(dim, inPts = inPts,random_seed = random_seed)
+            sampSpl = rand_spline(dim, nPts = nPts,random_seed = random_seed, endEdge = endEdge)
         else:
-            sampSpl = rand_spline(dim, nPts = 3,random_seed = random_seed)
+            sampSpl = rand_spline(dim, inPts = inPts,random_seed = random_seed)
     
     if samp_arr is None:
         samp_arr = np.copy(im_arr)
@@ -101,9 +182,9 @@ def add_fold(input_im,samp_arr =None, sampSpl=None, inPts = None,random_seed =No
         outBBPts[di,0] = rsSplBBox[LR_v_TB,0] + pad_szXY[0] + shiftXY[0] + randShiftX[di]
         outBBPts[di,1] = rsSplBBox[LT_V_RB,1] + pad_szXY[1] + shiftXY[1] + randShiftY[di]
 
-    M = cv2.getPerspectiveTransform(outBBPts,sampSplBBPts)
+    M = getPerspectiveTransform(outBBPts,sampSplBBPts)
 
-    warp_im = cv2.warpPerspective(samp_pad_arr,M,dim)
+    warp_im = warpPerspective(samp_pad_arr,M,dim)
 
     #
     mask = np.ones(dim)
@@ -111,20 +192,18 @@ def add_fold(input_im,samp_arr =None, sampSpl=None, inPts = None,random_seed =No
     bw_dist = morphology.distance_transform_edt(mask)
     if randEdge == True:
         distRand = np.random.randint(-int(fold_width/2),int(fold_width/2),size=invDim)
-        bw_dist = cv2.blur(bw_dist+distRand,(5,5))
+        bw_dist = blur(bw_dist+distRand,(5,5))
     
     im_L =  input_im.convert("L")
     im_L_arr = np.array(im_L)
     bw_reg = bw_dist <= fold_width
     
-#     alpha_arr = np.minimum(255,(((255-im_L_arr)*.25) * (bw_reg)))
-#     alpha_mask = Image.fromarray(alpha_arr.astype(np.uint8),'L')
 
     # multiplicative combination.  Makes things darker
     unit_dst_arr = np.ones(warp_im.shape)
     for i in range(warp_im.shape[2]):
         unit_dst_arr[:,:,i] = np.where(bw_reg,warp_im[:,:,i]/255,1)
-    unit_dst_arr = cv2.GaussianBlur(unit_dst_arr,sampBlur,0)
+    unit_dst_arr = GaussianBlur(unit_dst_arr,sampBlur,0)
     comb_arr = unit_dst_arr * im_arr
     comb_img = Image.fromarray(comb_arr.astype(np.uint8),'RGB')
     
@@ -133,8 +212,101 @@ def add_fold(input_im,samp_arr =None, sampSpl=None, inPts = None,random_seed =No
                  scaleXY=scaleXY,fold_width=fold_width,samp_shiftXY=samp_shiftXY,randEdge=randEdge,
                  nLayers=nLayers-1)
     return comb_img
+
+def add_sectioning(input_im, sliceWidth = 120, random_seed = None, scaleMin = .5, scaleMax = .8, randEdge = True,
+                  sampSpl = None, inPts = None):
+    # Uneven sectioning due to different thicknesses of slide
+    np.random.seed(seed=random_seed)
+    dim = input_im.size # width by height
+    invDim = (dim[1],dim[0]) # have to invert the size dim because rows cols is yx vs xy
     
-#     fig,ax = plt.subplots(figsize=(9, 6))
-#     plt.imshow(shifted_layer)
-#     plt.plot(sampSpl[:,0],sampSpl[:,1])
-#     plt.show()
+    if sampSpl is None:
+        if inPts is None:
+            sampSpl = rand_spline(dim, inPts = inPts,random_seed = random_seed)
+        else:
+            sampSpl = rand_spline(dim, nPts = 2, endEdge = -2,random_seed = random_seed)
+
+    mask = np.ones(dim)
+    mask[(sampSpl[:,1].astype(int)),sampSpl[:,0].astype(int)] = 0
+    bw_dist = morphology.distance_transform_edt(mask)
+    if randEdge == True:
+        distRand = np.random.randint(-int(sliceWidth),int(sliceWidth),size=invDim)
+        bw_dist = blur(bw_dist+distRand,(5,5))
+
+    bw_reg = bw_dist <= sliceWidth
+    nDistRng = bw_dist / sliceWidth
+    halfScale = (scaleMin + scaleMax)/2
+    scaleRandMin = np.random.uniform(scaleMin,(halfScale+scaleMin)/2,size=(1,1))
+    scaleRandMax = np.random.uniform((halfScale+scaleMax)/2,scaleMax,size=(1,1))
+    scaleRMinMax = np.concatenate((scaleRandMin,scaleRandMax),axis = 1).flatten()
+
+    nDistRng = np.interp(nDistRng,np.array([0,1],dtype=np.float64),scaleRMinMax)
+
+    nDistRng[np.logical_not(bw_reg)] = 1
+
+    imHSV = input_im.convert("HSV")
+    imHSV_arr = np.array(imHSV)
+    imHSV_arr[:,:,1] = np.minimum(255,np.multiply(imHSV_arr[:,:,1],nDistRng))
+    imHSV_arr[:,:,2] = np.minimum(255,np.divide(imHSV_arr[:,:,2],(nDistRng+1)/2))
+    # increase the lightness by half the factor of decreased saturation
+    imSatHSV = Image.fromarray(imHSV_arr,"HSV")
+    imSat = imSatHSV.convert("RGB")
+    return imSat
+
+def add_bubbles(input_im,random_seed = None,nBubbles = 25, maxWidth = 50,alpha = .75, edgeWidth = 2,
+               edgeColorMult = .75, rgbVal = (225,225,225)):
+    np.random.seed(seed=random_seed)
+    dim = input_im.size # width by height
+    invDim = (dim[1],dim[0]) # have to invert the size dim because rows cols is yx vs xy
+    maxCov = maxWidth^2
+    
+    sumMap = rand_gauss(dim,random_seed = random_seed, nNorms=nBubbles, maxCov = maxCov, zeroToOne = False,
+                       minCovScale = .1,minDiagCovScale = .25, maxCrCovScale = .7)
+    bw_reg = sumMap >= 1
+    # mask = 1-bw_reg # invert the mask
+    bw_dist = morphology.distance_transform_edt(bw_reg)
+    edge_area = np.logical_and(bw_dist <= edgeWidth,bw_reg)
+
+    alpha_mask = Image.fromarray((bw_reg*alpha*255).astype(np.uint8),'L')
+    color_arr = np.zeros((dim[0],dim[1],3),dtype=np.uint8)
+
+    meanColor = np.mean(np.array(input_im),axis=(0,1))
+    for i in range(len(rgbVal)):
+        color_arr[:,:,i] = rgbVal[i]
+        color_arr[edge_area,i] = np.uint8(meanColor[i] * edgeColorMult)
+
+    color_layer = Image.fromarray(color_arr,'RGB')
+    comp_im = Image.composite(color_layer, input_im, alpha_mask)
+    return comp_im
+
+def add_illumination(input_im,random_seed = None, maxCov = 15, nNorms = 3,scaleMin = .8,scaleMax = 1.1,
+                    minCovScale = .5,minDiagCovScale = .1, maxCrCovScale = .2):
+    np.random.seed(seed=random_seed)
+    dim = input_im.size # width by height
+    invDim = (dim[1],dim[0])
+
+    xV = np.linspace(0,1,num= dim[0])
+    yV = np.linspace(0,1,num= dim[1])
+    xM, yM = np.meshgrid(xV,yV)
+    pos = np.dstack((xM, yM))
+    
+    sumMap = rand_gauss(dim,random_seed = random_seed, nNorms=nNorms, maxCov = maxCov, 
+                        zeroToOne = True, minMaxX = [-.5,1.5],minMaxY = [-.5,1.5],
+                        minCovScale = minCovScale,minDiagCovScale = minDiagCovScale, maxCrCovScale = maxCrCovScale)
+
+    nSumMap = (sumMap - np.amin(sumMap,axis=(0,1)))
+    
+    divFac = np.amax(nSumMap,axis=(0,1))
+    nSumMap = nSumMap /divFac
+    
+    scaleRandMin = np.random.uniform(scaleMin,(1+scaleMin)/2,size=(1,1))
+    scaleRandMax = np.random.uniform((1+scaleMax)/2,scaleMax,size=(1,1))
+    scaleRMinMax = np.concatenate((scaleRandMin,scaleRandMax),axis = 1).flatten()
+    nSumMap = np.interp(nSumMap,np.array([0,1],dtype=np.float64),scaleRMinMax)
+    
+    imHSV = input_im.convert("HSV")
+    imHSV_arr = np.array(imHSV)
+    imHSV_arr[:,:,2] = np.minimum(255,np.multiply(imHSV_arr[:,:,2],nSumMap))
+    imLumHSV = Image.fromarray(imHSV_arr,"HSV")
+    imLum = imLumHSV.convert("RGB")
+    return imLum
